9313d3d1942e70a850fc7c22c8234076c8583183
shiren1118 <shiren1118@126.com>
2013-01-31 18:40:36 +0800

    sss

---
+++ examples/aaaa/test/Test.m
@@ -0,0 +1,17 @@
+//
+//  Test.m
+//  Helloworld
+//
+//  Created by sang on 1/29/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+-(void)hello{
+    NSLog(@"test.hello method called");
+}
+
+@end
